11d5db8d2631a972c69f24792352181fda6c194c
Muhammad Ranju <mdranju23@gmail.com>
2023-01-21 23:25:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ const PORT = process.env.PORT || 5400;
 const middlewares = [morgan("dev")];
 
 app.use(middlewares);
+const urlLinks = (req, res, next) => {
+    try {
+        console.log(req.params);
+        return next();
+    } catch (error) {
+        console.log(error);
+    }
+};
+const names = "name";
 
-app.get("/", (req, res) => {
-    res.redirect("/home");
+app.get(`/links/:id`, (req, res) => {
+    const link = `https://thebestdate.life/?u=568k60a&o=g1bpe97&t=${req.params.id}`;
+
+    console.log(link);
+    // res.redirect(link);
+    // res.redirect(`https://google.com?t=${req.params.id}`);
+    res.send(link);
 });
-app.get("/home", (req, res) => {
+app.get(`/links`, (req, res) => {
+    res.redirect(`/`);
+});
+app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello real app" });
 });
 
